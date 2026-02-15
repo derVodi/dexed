a87363f1eaf3b24f50b8cc8278de8ed616e5ec43
der Vodi <github@vodi.de>
2026-02-15 08:50:01 +0100

    Save Cartridge respects loaded file name

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -152,7 +152,7 @@ void DexedAudioProcessorEditor::loadCart(File file) {
 void DexedAudioProcessorEditor::saveCart() {
     File startFileName = processor->activeFileCartridge.exists() ? processor->activeFileCartridge : processor->dexedCartDir;
 
-    FileChooser fc ("Export DX sysex...", processor->dexedCartDir, "*.syx;*.SYX", 1);
+    FileChooser fc ("Export DX sysex...", startFileName, "*.syx;*.SYX", 1);
     if ( fc.browseForFileToSave(true) ) {
         if ( ! processor->currentCart.saveVoice(fc.getResults().getReference(0)) ) {
             AlertWindow::showMessageBoxAsync (AlertWindow::WarningIcon,
